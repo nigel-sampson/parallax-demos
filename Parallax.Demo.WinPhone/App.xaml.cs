3eb672ecf6e09cb9195f98e33400d67298245946
Nigel Sampson <nigel.sampson@compiledexperience.com>
2013-06-11 14:03:04 +1200

    Windows Phone Demo
    Demo of iOS 7 parallax background

---
+++ Parallax.Demo.WinPhone/App.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Parallax.Demo.WinPhone
+{
+    public partial class App
+    {
+        public static PhoneApplicationFrame RootFrame
+        {
+            get; private set;
+        }
+        public App()
+        {
+            UnhandledException += Application_UnhandledException;
+
+            InitializeComponent();
+
+            InitializePhoneApplication();
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+        }
+
+        #region Phone application initialization
+
+        // Avoid double-initialization
+        private bool _phoneApplicationInitialized;
+
+        // Do not add any additional code to this method
+        private void InitializePhoneApplication()
+        {
+            if (_phoneApplicationInitialized)
+                return;
+
+            RootFrame = new PhoneApplicationFrame();
+            RootFrame.Navigated += CompleteInitializePhoneApplication;
+            RootFrame.Navigated += CheckForResetNavigation;
+
+            _phoneApplicationInitialized = true;
+        }
+
+        private void CompleteInitializePhoneApplication(object sender, NavigationEventArgs e)
+        {
+            if (RootVisual != RootFrame)
+                RootVisual = RootFrame;
+
+            RootFrame.Navigated -= CompleteInitializePhoneApplication;
+        }
+
+        private void CheckForResetNavigation(object sender, NavigationEventArgs e)
+        {
+            if (e.NavigationMode == NavigationMode.Reset)
+                RootFrame.Navigated += ClearBackStackAfterReset;
+        }
+
+        private void ClearBackStackAfterReset(object sender, NavigationEventArgs e)
+        {
+            RootFrame.Navigated -= ClearBackStackAfterReset;
+
+            if (e.NavigationMode != NavigationMode.New && e.NavigationMode != NavigationMode.Refresh)
+                return;
+
+            while (RootFrame.RemoveBackEntry() != null)
+            {
+                ; // do nothing
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Parallax.Demo.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Applications.Common;
+
+namespace Parallax.Demo.WinPhone
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            AccelerometerHelper.Instance.Calibrate(true, true);
+
+            AccelerometerHelper.Instance.ReadingChanged += OnAccelerometerHelperReadingChanged;
+            AccelerometerHelper.Instance.Active = true;
+        }
+
+        private void OnAccelerometerHelperReadingChanged(object sender, AccelerometerHelperReadingEventArgs e)
+        {
+            var x = e.AverageAcceleration.X * -64.0;
+            var y = e.AverageAcceleration.Y * -64.0;
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                BackgroundTransform.X = Math.Max(-24, Math.Min(24, x));
+                BackgroundTransform.Y = Math.Max(-24, Math.Min(24, y));
+            });
+        }
+    }
+}
